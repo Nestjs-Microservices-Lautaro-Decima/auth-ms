5e71c60105fac53c7e91be1822cc6750d639dddb
Lautaro Decima <decimalautaro@gmail.com>
2024-09-14 20:05:01 -0300

    feat: configuring microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
+  const logger = new Logger('Auth-Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.NATS_SERVERS,
+      },
+    },
+  );
+  await app.listen();
+  logger.log(`Microservice running on port ${envs.PORT}`);
 }
 bootstrap();
