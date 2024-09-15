c09917d75f02dcabd4e99e7ea3a4893097434176
Lautaro Decima <decimalautaro@gmail.com>
2024-09-14 23:01:23 -0300

    feat: creating login and register controllers and services, also creating the JWT token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,25 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('AuthService');
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
 
   onModuleInit() {
     this.$connect();
     this.logger.log('MongoDB connected.');
+  }
+
+  async signJWT(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 
   async registerUser(registerUserDto: RegisterUserDto) {
@@ -33,13 +43,55 @@ export class AuthService extends PrismaClient implements OnModuleInit {
         data: {
           email: email,
           name: name,
-          password: password,
+          password: bcrypt.hashSync(password, 10),
         },
       });
 
+      const { password: __, ...rest } = newUser;
+
       return {
-        user: newUser,
-        token: 'ABC',
+        user: rest,
+        token: await this.signJWT(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    try {
+      const { email, password } = loginUserDto;
+
+      const user = await this.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid.',
+        });
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid.',
+        });
+      }
+
+      const { password: __, ...rest } = user;
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
       };
     } catch (error) {
       throw new RpcException({
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,14 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   NATS_SERVERS: string[];
+  JWT_SECRET: string;
 }
 
 const envsScheme = joi
   .object({
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
+    JWT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -27,4 +29,5 @@ const envVars: EnvVars = value;
 export const envs = {
   PORT: envVars.PORT,
   NATS_SERVERS: envVars.NATS_SERVERS,
+  JWT_SECRET: envVars.JWT_SECRET,
 };
